a70cae3661508bac0c9702201e5d2828d6a43ea4
Gildas Cherruel <gildas@breizh.org>
2022-03-04 16:43:48 +0900

    Use a test suite for Content unit testing

--- content_test.go
+++ content_test.go
@@ -3,181 +3,239 @@ package request_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"reflect"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/gildas/go-errors"
+	"github.com/gildas/go-logger"
 	"github.com/gildas/go-request"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestCanCreateContentWithURL(t *testing.T) {
+type ContentSuite struct {
+	suite.Suite
+	Name   string
+	Start  time.Time
+	Logger *logger.Logger
+}
+
+func TestContentSuite(t *testing.T) {
+	suite.Run(t, new(ContentSuite))
+}
+
+// *****************************************************************************
+// Suite Tools
+
+func (suite *ContentSuite) SetupSuite() {
+	_ = godotenv.Load()
+	suite.Name = strings.TrimSuffix(reflect.TypeOf(*suite).Name(), "Suite")
+	suite.Logger = logger.Create("test",
+		&logger.FileStream{
+			Path:        fmt.Sprintf("./log/test-%s.log", strings.ToLower(suite.Name)),
+			Unbuffered:  true,
+			FilterLevel: logger.TRACE,
+		},
+	).Child("test", "test")
+	suite.Logger.Infof("Suite Start: %s %s", suite.Name, strings.Repeat("=", 80-14-len(suite.Name)))
+}
+
+func (suite *ContentSuite) TearDownSuite() {
+	if suite.T().Failed() {
+		suite.Logger.Warnf("At least one test failed, we are not cleaning")
+		suite.T().Log("At least one test failed, we are not cleaning")
+	} else {
+		suite.Logger.Infof("All tests succeeded, we are cleaning")
+	}
+	suite.Logger.Infof("Suite End: %s %s", suite.Name, strings.Repeat("=", 80-12-len(suite.Name)))
+	suite.Logger.Close()
+}
+
+func (suite *ContentSuite) BeforeTest(suiteName, testName string) {
+	suite.Logger.Infof("Test Start: %s %s", testName, strings.Repeat("-", 80-13-len(testName)))
+	suite.Start = time.Now()
+}
+
+func (suite *ContentSuite) AfterTest(suiteName, testName string) {
+	duration := time.Since(suite.Start)
+	if suite.T().Failed() {
+		suite.Logger.Errorf("Test %s failed", testName)
+	}
+	suite.Logger.Record("duration", duration.String()).Infof("Test End: %s %s", testName, strings.Repeat("-", 80-11-len(testName)))
+}
+
+// *****************************************************************************
+
+func (suite *ContentSuite) TestCanCreateWithURL() {
 	data := []byte{1, 2, 3, 4, 5}
 	url, _ := url.Parse("https://www.acme.com")
 	content := request.ContentWithData(data, url)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
-	assert.Equal(t, url, content.URL)
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
+	suite.Assert().Equal(url, content.URL)
 }
 
-func TestCanCreateContentWithType(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateWithType() {
 	data := []byte{1, 2, 3, 4, 5}
 	mime := "image/png"
 	content := request.ContentWithData(data, mime)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
-	assert.Equal(t, mime, content.Type)
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
+	suite.Assert().Equal(mime, content.Type)
 }
 
-func TestCanCreateContentWithLength(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateWithLength() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data, len(data))
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
 }
 
-func TestCanCreateContentWithLength64(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateWithLength64() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data, int64(len(data)))
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
 }
 
-func TestCanCreateContentWithCookies(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateWithCookies() {
 	data := []byte{1, 2, 3, 4, 5}
 	url, _ := url.Parse("https://www.acme.com")
 	cookies := []*http.Cookie{{Name: "Test", Value: "1234", Secure: true, HttpOnly: true}}
 	content := request.ContentWithData(data, url, cookies)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
-	assert.Equal(t, url, content.URL)
-	assert.Equal(t, 1, len(content.Cookies))
-	assert.Equal(t, "Test", content.Cookies[0].Name)
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
+	suite.Assert().Equal(url, content.URL)
+	suite.Assert().Equal(1, len(content.Cookies))
+	suite.Assert().Equal("Test", content.Cookies[0].Name)
 }
 
-func TestCanCreateContentWithHeaders(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateWithHeaders() {
 	data := []byte{1, 2, 3, 4, 5}
 	url, _ := url.Parse("https://www.acme.com")
 	header := http.Header{}
 	header.Set("Custom-Header", "custom-value")
 	content := request.ContentWithData(data, url, header)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
-	assert.Equal(t, url, content.URL)
-	require.NotNil(t, content.Headers)
-	assert.Equal(t, "custom-value", content.Headers.Get("Custom-Header"))
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
+	suite.Assert().Equal(url, content.URL)
+	suite.Require().NotNil(content.Headers)
+	suite.Assert().Equal("custom-value", content.Headers.Get("Custom-Header"))
 }
 
-func TestCanCreateContentFromReader(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateFromReader() {
 	data := bytes.NewBuffer([]byte{1, 2, 3, 4, 5})
 	content, err := request.ContentFromReader(data)
-	require.Nil(t, err, "Failed to create Content, err=%+v", err)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(5), content.Length)
+	suite.Require().NoErrorf(err, "Failed to create Content, err=%+v", err)
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(5), content.Length)
 }
 
-func TestShouldFailCreateContentFromBogusReader(t *testing.T) {
+func (suite *ContentSuite) TestShouldFailCreateFromBogusReader() {
 	data := failingReader(0)
 	_, err := request.ContentFromReader(data)
-	require.NotNil(t, err, "Should fail create content")
+	suite.Require().Error(err, "Should fail create content")
 }
 
-func TestCanCreateReaderFromContent(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateReaderFromContent() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
 	reader := content.Reader()
-	require.NotNil(t, reader, "ContentReader should not be nil")
-	require.Implements(t, (*io.Reader)(nil), reader)
+	suite.Require().NotNil(reader, "ContentReader should not be nil")
+	suite.Require().Implements((*io.Reader)(nil), reader)
 }
 
-func TestCanCreateReadCloserFromContent(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateReadCloserFromContent() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, int64(len(data)), content.Length)
-	assert.Equal(t, data[0], content.Data[0])
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal(int64(len(data)), content.Length)
+	suite.Assert().Equal(data[0], content.Data[0])
 	reader := content.ReadCloser()
-	require.NotNil(t, reader, "ContentReader should not be nil")
-	require.Implements(t, (*io.ReadCloser)(nil), reader)
+	suite.Require().NotNil(reader, "ContentReader should not be nil")
+	suite.Require().Implements((*io.ReadCloser)(nil), reader)
 }
 
-func TestCanReadFromContentReader(t *testing.T) {
+func (suite *ContentSuite) TestCanReadFromContentReader() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
+	suite.Require().NotNil(content, "Content should not be nil")
 
 	content.Length = 0 // just to force the length to be computed again
 	reader := content.Reader()
-	require.NotNil(t, reader, "ContentReader should not be nil")
+	suite.Require().NotNil(reader, "ContentReader should not be nil")
 	length, err := reader.Read(content.Data)
-	require.Nil(t, err, "ContentReader should be able to read data, err=%+v", err)
-	assert.Equal(t, 5, length, "ContentReader should have read 5 bytes")
-	assert.Equal(t, data[0], content.Data[0])
+	suite.Require().NoErrorf(err, "ContentReader should be able to read data, err=%+v", err)
+	suite.Assert().Equal(5, length, "ContentReader should have read 5 bytes")
+	suite.Assert().Equal(data[0], content.Data[0])
 }
 
-func TestContentCanUnmarshallData(t *testing.T) {
+func (suite *ContentSuite) TestCanUnmarshallData() {
 	data := stuff{"12345"}
 	payload, _ := json.Marshal(data)
 	content := request.ContentWithData(payload)
-	require.NotNil(t, content, "Content should not be nil")
+	suite.Require().NotNil(content, "Content should not be nil")
 
 	value := stuff{}
 	err := content.UnmarshalContentJSON(&value)
-	require.Nil(t, err, "Content failed unmarshaling, err=%+v", err)
-	assert.Equal(t, data.ID, value.ID)
+	suite.Require().NoErrorf(err, "Content failed unmarshaling, err=%+v", err)
+	suite.Assert().Equal(data.ID, value.ID)
 }
 
-func TestShouldFailUnmarshallContentReaderWithBogusData(t *testing.T) {
+func (suite *ContentSuite) TestShouldFailUnmarshallContentWithBogusData() {
 	content := request.ContentWithData([]byte(`{"ID": 1234}`), "application/json")
 	data := stuff{}
 	err := content.UnmarshalContentJSON(&data)
-	require.NotNil(t, err, "Should fail unmarshal content")
-	assert.Truef(t, errors.Is(err, errors.JSONUnmarshalError), "Error should be a JSON Unmarshal Error")
+	suite.Require().Error(err, "Should fail unmarshal content")
+	suite.Assert().Truef(errors.Is(err, errors.JSONUnmarshalError), "Error should be a JSON Unmarshal Error")
 	var details errors.Error
-	require.True(t, errors.As(err, &details), "Error chain should contain an errors.Error")
-	assert.Equal(t, "error.json.unmarshal", details.ID, "Error's ID is wrong (%s)", details.ID)
+	suite.Require().True(errors.As(err, &details), "Error chain should contain an errors.Error")
+	suite.Assert().Equal("error.json.unmarshal", details.ID, "Error's ID is wrong (%s)", details.ID)
 }
 
-func TestContentShouldLogBinaryContent(t *testing.T) {
+func (suite *ContentSuite) TestShouldLogBinaryContent() {
 	data := []byte{1, 2, 3, 4, 5}
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
+	suite.Require().NotNil(content, "Content should not be nil")
 	content.Type = "image/png"
 
-	assert.Equal(t, "image/png, 5 bytes: 0102030405", content.LogString(10))
+	suite.Assert().Equal("image/png, 5 bytes: 0102030405", content.LogString(10))
 }
 
-func TestContentShouldLogTextContent(t *testing.T) {
+func (suite *ContentSuite) TestShouldLogTextContent() {
 	data := []byte("Hello")
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
+	suite.Require().NotNil(content, "Content should not be nil")
 	content.Type = "text/plain"
 
-	assert.Equal(t, "text/plain, 5 bytes: Hello", content.LogString(10))
+	suite.Assert().Equal("text/plain, 5 bytes: Hello", content.LogString(10))
 }
 
-func TestContentShouldLogJSONContent(t *testing.T) {
+func (suite *ContentSuite) TestShouldLogJSONContent() {
 	data := []byte(`{"data": "Hello"}`)
 	content := request.ContentWithData(data)
-	require.NotNil(t, content, "Content should not be nil")
+	suite.Require().NotNil(content, "Content should not be nil")
 	content.Type = "application/json"
 
-	assert.Equal(t, `application/json, 17 bytes: {"data": "Hello"}`, content.LogString(20))
+	suite.Assert().Equal(`application/json, 17 bytes: {"data": "Hello"}`, content.LogString(20))
 }
 
-func TestCanCreateContentFromCompressedData(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateFromCompressedData() {
 	data := []byte{
 		31, 139, 8, 0, 0, 0, 0, 0, 2, 255, 69, 142, 61, 15, 130, 64, 12, 134, 119, 126, 69, 195, 172,
 		23, 7, 38, 86, 212, 17, 22, 157, 77, 229, 10, 185, 8, 215, 179, 119, 136, 9, 225, 191, 91, 162,
@@ -200,13 +258,13 @@ func TestCanCreateContentFromCompressedData(t *testing.T) {
 	headers.Set("Content-Encoding", "gzip")
 	headers.Set("Content-Type", "application/json")
 	content := request.ContentWithData(data, len(data), "application/json", headers)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, "application/json", content.Type)
-	assert.Equalf(t, len(expected), int(content.Length), "Content length should be %d", len(expected))
-	assert.Equal(t, expected, string(content.Data))
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal("application/json", content.Type)
+	suite.Assert().Equalf(len(expected), int(content.Length), "Content length should be %d", len(expected))
+	suite.Assert().Equal(expected, string(content.Data))
 }
 
-func TestCanCreateContentFromBogusCompressedData(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateFromBogusCompressedData() {
 	data := []byte{
 		31, 139, 8, 0, 0, 0, 0, 0, 2, 255, 69, 142, 61, 15, 130, 64, 12, 134, 119, 126, 69, 195, 172,
 		23, 7, 38, 86, 212, 17, 22, 157, 77, 229, 10, 185, 8, 215, 179, 119, 136, 9, 225, 191, 91, 162,
@@ -218,12 +276,12 @@ func TestCanCreateContentFromBogusCompressedData(t *testing.T) {
 	headers.Set("Content-Encoding", "gzip")
 	headers.Set("Content-Type", "application/json")
 	content := request.ContentWithData(data, len(data), "application/json", headers)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, "application/json", content.Type)
-	assert.Equalf(t, len(data), int(content.Length), "Content length should be %d", len(data))
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal("application/json", content.Type)
+	suite.Assert().Equalf(len(data), int(content.Length), "Content length should be %d", len(data))
 }
 
-func TestCanCreateContentFromBogusCompressedDataHeader(t *testing.T) {
+func (suite *ContentSuite) TestCanCreateFromBogusCompressedDataHeader() {
 	data := []byte{
 		23, 7, 38, 86, 212, 17, 22, 157, 77, 229, 10, 185, 8, 215, 179, 119, 136, 9, 225, 191, 91, 162,
 		137, 75, 147, 231, 125, 250, 181, 100, 0, 57, 137, 176, 228, 37, 44, 10, 138, 45, 91, 82, 42,
@@ -237,7 +295,7 @@ func TestCanCreateContentFromBogusCompressedDataHeader(t *testing.T) {
 	headers.Set("Content-Encoding", "gzip")
 	headers.Set("Content-Type", "application/json")
 	content := request.ContentWithData(data, len(data), "application/json", headers)
-	require.NotNil(t, content, "Content should not be nil")
-	assert.Equal(t, "application/json", content.Type)
-	assert.Equalf(t, len(data), int(content.Length), "Content length should be %d", len(data))
+	suite.Require().NotNil(content, "Content should not be nil")
+	suite.Assert().Equal("application/json", content.Type)
+	suite.Assert().Equalf(len(data), int(content.Length), "Content length should be %d", len(data))
 }
