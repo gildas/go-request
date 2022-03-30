efd2dc66803eabdaffda8cb703d92e0a2c836188
Gildas Cherruel <gildas@breizh.org>
2022-03-30 09:13:13 +0900

    Reflect should be used as less as possible

--- request.go
+++ request.go
@@ -287,7 +287,6 @@ func buildRequestContent(log *logger.Logger, options *Options) (content *Content
 		}
 	}
 
-	payloadType := reflect.TypeOf(options.Payload)
 	if _content, ok := options.Payload.(Content); ok {
 		log.Tracef("Payload is a Content (Type: %s, size: %d)", _content.Type, _content.Length)
 		if len(_content.Type) == 0 {
@@ -311,108 +310,111 @@ func buildRequestContent(log *logger.Logger, options *Options) (content *Content
 	} else if reader, ok := options.Payload.(io.Reader); ok {
 		log.Tracef("Payload is a Reader (Data Type: %s)", options.PayloadType)
 		content, _ = ContentFromReader(reader, options.PayloadType, 0, nil, nil)
-	} else if payloadType.Kind() == reflect.Struct || (payloadType.Kind() == reflect.Ptr && reflect.Indirect(reflect.ValueOf(options.Payload)).Kind() == reflect.Struct) { // JSONify the payload
-		log.Tracef("Payload is a Struct, JSONifying it")
-		// TODO: Add other payload types like XML, etc
-		if len(options.PayloadType) == 0 {
-			options.PayloadType = "application/json"
-		}
-		payload, err := json.Marshal(options.Payload)
-		if err != nil {
-			if errors.Is(err, errors.JSONMarshalError) {
-				return nil, err
-			}
-			return nil, errors.JSONMarshalError.Wrap(err)
-		}
-		content = ContentWithData(payload, options.PayloadType)
-	} else if payloadType.Kind() == reflect.Array || payloadType.Kind() == reflect.Slice {
-		log.Tracef("Payload is an array or a slice, JSONifying it")
-		// TODO: Add other payload types like XML, etc
-		if len(options.PayloadType) == 0 {
-			options.PayloadType = "application/json"
-		}
-		payload, err := json.Marshal(options.Payload)
-		if err != nil {
-			if errors.Is(err, errors.JSONMarshalError) {
-				return nil, err
-			}
-			return nil, errors.JSONMarshalError.Wrap(err)
-		}
-		content = ContentWithData(payload, options.PayloadType)
-	} else if payloadType.Kind() == reflect.Map {
-		// Collect the attributes from the map
-		attributes := map[string]string{}
-		if stringMap, ok := options.Payload.(map[string]string); ok {
-			log.Tracef("Payload is a StringMap")
-			attributes = stringMap
-		} else { // traverse the map, collecting values if they are Stringer. Note: This can be slow...
-			log.Tracef("Payload is a Map")
-			items := reflect.ValueOf(options.Payload)
-			for _, item := range items.MapKeys() {
-				value := items.MapIndex(item)
-				if stringer, ok := value.Interface().(fmt.Stringer); ok {
-					attributes[item.String()] = stringer.String()
-				}
-			}
-		}
-
-		// Build the content as a Form or a Multipart Data Form
-		if options.Attachment == nil {
-			log.Tracef("Building a form (no attachment)")
+	} else {
+		payloadType := reflect.TypeOf(options.Payload)
+		if payloadType.Kind() == reflect.Struct || (payloadType.Kind() == reflect.Ptr && reflect.Indirect(reflect.ValueOf(options.Payload)).Kind() == reflect.Struct) { // JSONify the payload
+			log.Tracef("Payload is a Struct, JSONifying it")
+			// TODO: Add other payload types like XML, etc
 			if len(options.PayloadType) == 0 {
-				options.PayloadType = "application/x-www-form-urlencoded"
+				options.PayloadType = "application/json"
 			}
-			form := url.Values{}
-			for key, value := range attributes {
-				form.Set(key, value)
+			payload, err := json.Marshal(options.Payload)
+			if err != nil {
+				if errors.Is(err, errors.JSONMarshalError) {
+					return nil, err
+				}
+				return nil, errors.JSONMarshalError.Wrap(err)
 			}
-			return ContentWithData([]byte(form.Encode()), options.PayloadType), nil
-		}
+			content = ContentWithData(payload, options.PayloadType)
+		} else if payloadType.Kind() == reflect.Array || payloadType.Kind() == reflect.Slice {
+			log.Tracef("Payload is an array or a slice, JSONifying it")
+			// TODO: Add other payload types like XML, etc
+			if len(options.PayloadType) == 0 {
+				options.PayloadType = "application/json"
+			}
+			payload, err := json.Marshal(options.Payload)
+			if err != nil {
+				if errors.Is(err, errors.JSONMarshalError) {
+					return nil, err
+				}
+				return nil, errors.JSONMarshalError.Wrap(err)
+			}
+			content = ContentWithData(payload, options.PayloadType)
+		} else if payloadType.Kind() == reflect.Map {
+			// Collect the attributes from the map
+			attributes := map[string]string{}
+			if stringMap, ok := options.Payload.(map[string]string); ok {
+				log.Tracef("Payload is a StringMap")
+				attributes = stringMap
+			} else { // traverse the map, collecting values if they are Stringer. Note: This can be slow...
+				log.Tracef("Payload is a Map")
+				items := reflect.ValueOf(options.Payload)
+				for _, item := range items.MapKeys() {
+					value := items.MapIndex(item)
+					if stringer, ok := value.Interface().(fmt.Stringer); ok {
+						attributes[item.String()] = stringer.String()
+					}
+				}
+			}
 
-		log.Tracef("Building a multipart data form with 1 attachment")
-		body := &bytes.Buffer{}
-		writer := multipart.NewWriter(body)
-		for key, value := range attributes {
-			if strings.HasPrefix(key, ">") {
-				key = strings.TrimPrefix(key, ">")
-				if len(key) == 0 {
-					return nil, errors.Errorf("Empty key for multipart form field with attachment")
+			// Build the content as a Form or a Multipart Data Form
+			if options.Attachment == nil {
+				log.Tracef("Building a form (no attachment)")
+				if len(options.PayloadType) == 0 {
+					options.PayloadType = "application/x-www-form-urlencoded"
 				}
-				if len(value) == 0 {
-					return nil, errors.Errorf("Empty value for multipart form field %s", key)
+				form := url.Values{}
+				for key, value := range attributes {
+					form.Set(key, value)
 				}
-				partHeader := textproto.MIMEHeader{}
-				partHeader.Add("Content-Disposition", fmt.Sprintf("form-data; name=\"%s\"; filename=\"%s\"", key, value))
-				if len(options.AttachmentType) > 0 {
-					partHeader.Add("Content-Type", options.AttachmentType)
-				}
-				part, err := writer.CreatePart(partHeader)
-				if err != nil {
-					return nil, errors.Wrapf(err, "Failed to create multipart for field %s", key)
-				}
-				_, err = options.Attachment.(io.Seeker).Seek(0, io.SeekStart)
-				if err != nil {
-					return nil, errors.Wrapf(err, "Failed to seek to beginning of attachment for field %s", key)
-				}
-				written, err := io.Copy(part, options.Attachment)
-				if err != nil {
-					return nil, errors.Errorf("Failed to write attachment to multipart form field %s", key)
-				}
-				if written == 0 {
-					return nil, errors.Errorf("Missing/Empty Attachment for multipart form field %s", key)
-				}
-				log.Tracef("Wrote %d bytes to multipart form field %s", written, key)
-			} else {
-				if err := writer.WriteField(key, value); err != nil {
-					return nil, errors.Wrapf(err, "Failed to create multipart form field %s", key)
-				}
-				log.Tracef("  Added field %s = %s", key, value)
+				return ContentWithData([]byte(form.Encode()), options.PayloadType), nil
 			}
+
+			log.Tracef("Building a multipart data form with 1 attachment")
+			body := &bytes.Buffer{}
+			writer := multipart.NewWriter(body)
+			for key, value := range attributes {
+				if strings.HasPrefix(key, ">") {
+					key = strings.TrimPrefix(key, ">")
+					if len(key) == 0 {
+						return nil, errors.Errorf("Empty key for multipart form field with attachment")
+					}
+					if len(value) == 0 {
+						return nil, errors.Errorf("Empty value for multipart form field %s", key)
+					}
+					partHeader := textproto.MIMEHeader{}
+					partHeader.Add("Content-Disposition", fmt.Sprintf("form-data; name=\"%s\"; filename=\"%s\"", key, value))
+					if len(options.AttachmentType) > 0 {
+						partHeader.Add("Content-Type", options.AttachmentType)
+					}
+					part, err := writer.CreatePart(partHeader)
+					if err != nil {
+						return nil, errors.Wrapf(err, "Failed to create multipart for field %s", key)
+					}
+					_, err = options.Attachment.(io.Seeker).Seek(0, io.SeekStart)
+					if err != nil {
+						return nil, errors.Wrapf(err, "Failed to seek to beginning of attachment for field %s", key)
+					}
+					written, err := io.Copy(part, options.Attachment)
+					if err != nil {
+						return nil, errors.Errorf("Failed to write attachment to multipart form field %s", key)
+					}
+					if written == 0 {
+						return nil, errors.Errorf("Missing/Empty Attachment for multipart form field %s", key)
+					}
+					log.Tracef("Wrote %d bytes to multipart form field %s", written, key)
+				} else {
+					if err := writer.WriteField(key, value); err != nil {
+						return nil, errors.Wrapf(err, "Failed to create multipart form field %s", key)
+					}
+					log.Tracef("  Added field %s = %s", key, value)
+				}
+			}
+			if err := writer.Close(); err != nil {
+				return nil, errors.Wrap(err, "Failed to create multipart data")
+			}
+			content, _ = ContentFromReader(body, writer.FormDataContentType())
 		}
-		if err := writer.Close(); err != nil {
-			return nil, errors.Wrap(err, "Failed to create multipart data")
-		}
-		content, _ = ContentFromReader(body, writer.FormDataContentType())
 	}
 	if content != nil {
 		if options.RequestBodyLogSize > 0 {
