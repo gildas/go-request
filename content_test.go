f837e36802fb8843e38feee2b78cba9a192ae308
Gildas Cherruel <gildas@breizh.org>
2022-12-08 15:06:50 +0900

    Go fmt

--- content_test.go
+++ content_test.go
@@ -40,8 +40,8 @@ func (suite *ContentSuite) SetupSuite() {
 	suite.Name = strings.TrimSuffix(reflect.TypeOf(*suite).Name(), "Suite")
 	suite.Logger = logger.Create("test",
 		&logger.FileStream{
-			Path:        fmt.Sprintf("./log/test-%s.log", strings.ToLower(suite.Name)),
-			Unbuffered:  true,
+			Path:         fmt.Sprintf("./log/test-%s.log", strings.ToLower(suite.Name)),
+			Unbuffered:   true,
 			SourceInfo:   true,
 			FilterLevels: logger.NewLevelSet(logger.TRACE),
 		},
